cd20990fff8b72ef605e0b25e6a5c25ab3fec7a0
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2022-01-21 13:26:02 -0500

    Work on the Recursive type

--- morphir-core/shared/src/main/scala/zio/morphir/trees/Recursive.scala
+++ morphir-core/shared/src/main/scala/zio/morphir/trees/Recursive.scala
@@ -2,42 +2,54 @@ package zio.morphir.trees
 
 import zio.prelude._
 
-final case class Recursive[Case[+_]](caseValue: Case[Recursive[Case]]) {
+final case class Recursive[Case[+_], +Anns](caseValue: Case[Recursive[Case, Anns]], anns: AnnotationMap[Anns]) {
 
   def fold[Z](f: Case[Z] => Z)(implicit covariant: Covariant[Case]): Z =
     f(caseValue.map(_.fold(f)))
 
-  /**
-   * Folds over the recursive data structure to reduce it to a summary value, providing access to the recursive
-   * structure annotated with the current previous summary values in each step of the fold.
-   */
-  def foldAnnotated[Z](f: Case[Annotated[Case, Z]] => Z)(implicit covariant: Covariant[Case]): Z = {
-    def annotate(recursive: Recursive[Case]): Annotated[Case, Z] =
-      Annotated(recursive.caseValue.map(annotate), recursive.foldAnnotated(f))
-    f(caseValue.map(annotate))
-  }
+  def foldAnns[Z](f: (Case[Z], AnnotationMap[Anns]) => Z)(implicit covariant: Covariant[Case]): Z =
+    f(caseValue.map(_.foldAnns(f)), anns)
+
+//   /**
+//    * i Folds over the recursive data structure to reduce it to a summary value, providing access to the recursive
+//    * structure annotated with the current previous summary values in each step of the fold.
+//    */
+//   def foldAnnotated[Z](f: Case[Annotated[Case, Z]] => Z)(implicit covariant: Covariant[Case]): Z = {
+//     def annotate(recursive: Recursive[Case, Anns]): Annotated[Case, Z] =
+//       Annotated(recursive.caseValue.map(annotate), recursive.foldAnnotated(f))
+//     f(caseValue.map(annotate))
+//   }
 
   def foldM[F[+_]: AssociativeFlatten: Covariant: IdentityBoth, Z](f: Case[Z] => F[Z])(implicit
       foreach: ForEach[Case]
   ): F[Z] =
     fold[F[Z]](_.flip.flatMap(f))
 
-  def foldRecursive[Z](f: Case[(Recursive[Case], Z)] => Z)(implicit covariant: Covariant[Case]): Z =
+  def foldRecursive[Z](f: Case[(Recursive[Case, Anns], Z)] => Z)(implicit covariant: Covariant[Case]): Z =
     f(caseValue.map(recursive => recursive -> recursive.foldRecursive(f)))
 }
 
 object Recursive {
 
-  def unfold[Case[+_], Z](z: Z)(f: Z => Case[Z])(implicit covariant: Covariant[Case]): Recursive[Case] =
-    Recursive(f(z).map(unfold(_)(f)))
+  def apply[Case[+_]](caseValue: Case[Recursive[Case, Any]]): Recursive[Case, Any] =
+    Recursive(caseValue, AnnotationMap.empty)
 
-  def unfoldRecursive[Case[+_], Z](z: Z)(
-      f: Either[Recursive[Case], Z] => Case[Either[Recursive[Case], Z]]
-  )(implicit covariant: Covariant[Case]): Recursive[Case] =
-    Recursive {
-      f(Right(z)).map {
-        case Left(recursive) => recursive
-        case Right(z)        => unfoldRecursive(z)(f)
-      }
-    }
+  def unfold[Case[+_], Anns, Z](
+      z: Z
+  )(f: Z => (Case[Z], AnnotationMap[Anns]))(implicit covariant: Covariant[Case]): Recursive[Case, Anns] = {
+    val (caseValue, anns) = f(z)
+    Recursive(caseValue.map { zed => unfold(zed)(f) }, anns)
+  }
+
+  def unfoldRecursive[Case[+_], Anns, Z](z: Z)(
+      f: Either[Recursive[Case, Anns], Z] => Case[Either[Recursive[Case, Anns], Z]]
+  )(implicit covariant: Covariant[Case]): Recursive[Case, Anns] = {
+    // Recursive {
+    //   f(Right(z)).map {
+    //     case Left(recursive) => recursive
+    //     case Right(z)        => unfoldRecursive(z)(f)
+    //   }
+    // }
+    ???
+  }
 }
