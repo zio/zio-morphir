c027a28082396307a65ad906f6307df61c6f42ae
Chan, Michelle <6752211+michelchan@users.noreply.github.com>
2022-03-02 15:02:56 -0500

    adding test cases

--- morphir-ir/shared/src/test/scala/zio/morphir/ir/ValueModuleSpec.scala
+++ morphir-ir/shared/src/test/scala/zio/morphir/ir/ValueModuleSpec.scala
@@ -20,6 +20,49 @@ object ValueModuleSpec extends MorphirBaseSpec with ValueSyntax {
         assertTrue(
           apply(ff, rec).collectVariables == expected
         )
+      },
+      test("Constructor") {
+        val fqName = zio.morphir.ir.FQName(
+          zio.morphir.ir.Path(Name("Morphir.SDK")),
+          zio.morphir.ir.Path(Name("Morphir.SDK")),
+          Name("RecordType")
+        )
+        val constr = constructor(fqName)
+        assertTrue(constr.collectVariables == Set())
+      },
+      test("Destructure") {
+        val des = destructure(
+          tuplePattern(asPattern(wildcardPattern, Name("name1")), asPattern(wildcardPattern, Name("name2"))),
+          tuple(string("red"), string("blue")),
+          variable("x")
+        )
+        assertTrue(des.collectVariables == Set(Name("x")))
+      },
+      test("Field") {
+        val name = Name.fromString("Name")
+        val fi   = field(string("String"), name)
+
+        val name2 = Name.fromString("Name2")
+        val name3 = Name.fromString("Name3")
+        val fi2   = field(fieldFunction(name2), name3)
+
+        assertTrue(
+          fi.collectVariables == Set() &&
+            fi2.collectVariables == Set(name2)
+        )
+      },
+      test("FieldFunction") {
+        val name = Name.fromString("Name")
+        val ff   = fieldFunction(name)
+        assertTrue(ff.collectVariables == Set(name))
+      },
+      test("IfThenElse") {
+        val ife = ifThenElse(
+          condition = literal(false),
+          thenBranch = variable("y"),
+          elseBranch = literal(3)
+        )
+        assertTrue(ife.collectVariables == Set(Name.fromString("y")))
       }
 //      test("Constructor") {},
 //      test("Destructure") {},
