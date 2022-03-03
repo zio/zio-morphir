e1d3d96ca0569acc654e125c85bb60458026b858
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2022-03-03 09:57:57 -0500

    Working through issues

--- scalafix/input/src/main/scala/fix/AddModuleTypeSpec.scala
+++ scalafix/input/src/main/scala/fix/AddModuleTypeSpec.scala
@@ -6,3 +6,9 @@ package zio.morphir.sdk
 import zio.morphir.module
 
 @module(namespace = Some("Morphir.SDK")) object Bool {}
+
+@module(namespace = Some("Morphir.SDK"), name = Some("Basics")) object BasicsModule {}
+
+package foo.bar {
+  object Foo
+}
--- scalafix/output/src/main/scala/fix/AddModuleTypeSpec.scala
+++ scalafix/output/src/main/scala/fix/AddModuleTypeSpec.scala
@@ -2,4 +2,6 @@ package zio.morphir.sdk
 
 import zio.morphir.module
 
-@module(namespace = Some("Morphir.SDK")) object Bool {}
+@module(namespace = Some("Morphir.SDK")) object Bool {
+  val moduleSpec = "Morphir.SDK.Bool"
+}
