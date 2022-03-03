979af88162826b1faee2f17bdf8afad7a239ea4e
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2022-03-03 10:47:02 -0500

    Changes

--- scalafix/input/src/main/scala/fix/AddModuleTypeSpec.scala
+++ scalafix/input/src/main/scala/fix/AddModuleTypeSpec.scala
@@ -3,8 +3,6 @@ rule = AddModuleTypeSpec
  */
 package zio.morphir.sdk
 
-import zio.morphir.module
-
 @module(namespace = Some("Morphir.SDK")) object Bool {}
 
 @module(namespace = Some("Morphir.SDK"), name = Some("Basics")) object BasicsModule {}
