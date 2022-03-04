6db73d362cbd9f8430e92e1e7e43ca611510e508
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2022-03-04 15:05:22 -0500

    Working towards JSON encoding

--- morphir-json/shared/src/main/scala/zio/morphir/json/Codecs.scala
+++ morphir-json/shared/src/main/scala/zio/morphir/json/Codecs.scala
@@ -1,25 +1,29 @@
 package zio.morphir.json
-import zio.Chunk
 import zio.json._
-import zio.morphir.ir.{MorphirIRVersion, Name}
-import zio.morphir.ir.ValueModule.Value
-import zio.morphir.ir.codec._
+import zio.json.internal.Write
+import zio.morphir.ir.TypeModule.Type
+import zio.morphir.ir.ValueModule.ValueCase.UnitCase
+import zio.morphir.ir.ValueModule.{Value, ValueCase}
+import zio.morphir.ir.{ModuleDefinition, Name, PackageDefinition}
 
 
-trait JsonSupport {
+trait MorphirJsonCodecV1 {
+
+  // NOTE: We will want to create JSON encoders which follow the format in the morphir-elm project
   implicit val unitEncoder:JsonEncoder[Unit] = JsonEncoder.list[String].contramap(_ => List.empty[String])
   implicit val nameEncoder:JsonEncoder[Name] = JsonEncoder.list[String].contramap(name => name.toList)
 
-  implicit def valueEncoder[Annotations](implicit annotationsEncoder:JsonEncoder[Annotations]):MorphirEncoder[Value[Annotations]] = new MorphirEncoder[Value[Annotations]] {
-    import zio.morphir.ir.ValueModule.ValueCase._
-    override def encode(value: Value[Annotations])(implicit options: CodecOptions): CharSequence = options.morphirIRVersion match {
-      case MorphirIRVersion.V1_0 => value.fold[CharSequence]{
-        case UnitCase => ("unit", ).toJson
-        case _ => ???
-      }
-    }
+  implicit def valueEncoder[Annotations](implicit annotationsEncoder:JsonEncoder[Annotations]):JsonEncoder[Value[Annotations]] = ???
+  implicit def typeEncoder[Annotations](implicit annotationsEncoder:JsonEncoder[Annotations]):JsonEncoder[Type[Annotations]] = ???
+
+  def encodePackageDefinition[Annotations](packageDefinition:PackageDefinition[Annotations])(implicit annotationEncoder:JsonEncoder[Annotations]):CharSequence = {
+    // TODO: Look here for hints at implementing: https://github.com/finos/morphir-elm/blob/9122b99a6f500af389bfa26c2f7c604ea64485c2/src/Morphir/IR/Package/CodecV1.elm
+    ???
   }
+
+  def encodeModuleDefinition[Annotations](moduleDef:ModuleDefinition[Annotations])(implicit annotationEncoder:JsonEncoder[Annotations]):CharSequence = ???
+
 }
 
-object JsonSupport extends JsonSupport
+object MorphirJsonCodecV1 extends MorphirJsonCodecV1
 
