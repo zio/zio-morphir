4d655c2d3465edd9f0a74a90c30f50249e38405d
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2022-03-04 17:37:52 -0500

    Basics of encoding a type
    Co-authored-by: Adam Fraser <adam.fraser@gmail.com>
    Co-authored-by: Dmitry Kozinets <dmitrykozinets@users.noreply.github.com>

--- morphir-json/shared/src/main/scala/zio/morphir/json/Codecs.scala
+++ morphir-json/shared/src/main/scala/zio/morphir/json/Codecs.scala
@@ -1,7 +1,10 @@
 package zio.morphir.json
+import zio.ZEnvironment
 import zio.json._
+import zio.json.ast.Json
 import zio.json.internal.Write
-import zio.morphir.ir.TypeModule.Type
+import zio.morphir.ir.{FQName}
+import zio.morphir.ir.TypeModule.{Field, Type, TypeCase}
 import zio.morphir.ir.ValueModule.ValueCase.UnitCase
 import zio.morphir.ir.ValueModule.{Value, ValueCase}
 import zio.morphir.ir.{ModuleDefinition, Name, PackageDefinition}
@@ -9,15 +12,50 @@ import zio.morphir.ir.{ModuleDefinition, Name, PackageDefinition}
 trait MorphirJsonCodecV1 {
 
   // NOTE: We will want to create JSON encoders which follow the format in the morphir-elm project
-  implicit val unitEncoder: JsonEncoder[Unit] = JsonEncoder.list[String].contramap(_ => List.empty[String])
-  implicit val nameEncoder: JsonEncoder[Name] = JsonEncoder.list[String].contramap(name => name.toList)
+  implicit val unitEncoder: JsonEncoder[Unit]     = JsonEncoder.list[String].contramap(_ => List.empty[String])
+  implicit val nameEncoder: JsonEncoder[Name]     = JsonEncoder.list[String].contramap(name => name.toList)
+  implicit val fqNameEncoder: JsonEncoder[FQName] = ???
+
+  implicit def fieldEncoder[A](implicit
+      annotationsEncoder: JsonEncoder[A]
+  ): JsonEncoder[Field[A]] = ???
+  // JsonEncoder.list[(String, Json)]
+  //   .contramap(field => field.name.toList.map(name => (name, field.value.toJson)))
 
   implicit def valueEncoder[Annotations](implicit
       annotationsEncoder: JsonEncoder[Annotations]
   ): JsonEncoder[Value[Annotations]] = ???
+
   implicit def typeEncoder[Annotations](implicit
-      annotationsEncoder: JsonEncoder[Annotations]
-  ): JsonEncoder[Type[Annotations]] = ???
+      annotationsEncoder: JsonEncoder[ZEnvironment[Annotations]]
+  ): JsonEncoder[Type[Annotations]] =
+    Json.encoder.contramap[Type[Annotations]] { tpe =>
+      tpe.foldAnnotated[Json] {
+        case (TypeCase.ExtensibleRecordCase(name, fields), annotations) =>
+          Json.Arr(
+            Json.Str("extensible_record"),
+            toJsonAstOrThrow(annotations),
+            toJsonAstOrThrow(name),
+            Json.Arr(fields.map(_.toJsonAST.right.get))
+          )
+        case (TypeCase.FunctionCase(paramTypes, returnType), annotations) =>
+          Json.Arr(Json.Str("function"), toJsonAstOrThrow(annotations), Json.Arr(paramTypes), returnType, ???)
+        case (TypeCase.RecordCase(fields), annotations) =>
+          Json.Arr(Json.Str("record"), toJsonAstOrThrow(annotations), Json.Arr(fields.map(_.toJsonAST.right.get)))
+        case (TypeCase.ReferenceCase(typeName, typeParameters), annotations) =>
+          Json.Arr(
+            Json.Str("reference"),
+            toJsonAstOrThrow(annotations),
+            typeName.toJsonAST.right.get,
+            Json.Arr(typeParameters)
+          )
+        case (TypeCase.TupleCase(items), annotations) =>
+          Json.Arr(Json.Str("tuple"), toJsonAstOrThrow(annotations), Json.Arr(items))
+        case (TypeCase.UnitCase, annotations) => Json.Arr(Json.Str("unit"), annotations.toJsonAST.right.get)
+        case (TypeCase.VariableCase(name), annotations) =>
+          Json.Arr(Json.Str("variable"), toJsonAstOrThrow(annotations), toJsonAstOrThrow(name))
+      }
+    }
 
   def encodePackageDefinition[Annotations](
       packageDefinition: PackageDefinition[Annotations]
@@ -30,6 +68,8 @@ trait MorphirJsonCodecV1 {
       annotationEncoder: JsonEncoder[Annotations]
   ): CharSequence = ???
 
+  private def toJsonAstOrThrow[A](a: A)(implicit encoder: JsonEncoder[A]): Json =
+    a.toJsonAST.right.get
 }
 
 object MorphirJsonCodecV1 extends MorphirJsonCodecV1
