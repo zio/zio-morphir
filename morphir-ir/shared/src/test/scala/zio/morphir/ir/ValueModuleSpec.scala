161eb54802c146cb9714553a9726ab59241ff7c6
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2022-04-03 14:58:02 -0400

    Remove -Ywarn-value-discard and --no-indent

--- morphir-ir/shared/src/test/scala/zio/morphir/ir/ValueModuleSpec.scala
+++ morphir-ir/shared/src/test/scala/zio/morphir/ir/ValueModuleSpec.scala
@@ -10,7 +10,6 @@ import zio.morphir.testing.MorphirBaseSpec
 // import zio.test.TestAspect.{ignore, tag}
 import zio.test._
 import zio.morphir.ir.Type.{Type => IrType, UType}
-import scala.annotation.nowarn
 
 object ValueModuleSpec extends MorphirBaseSpec with value.ValueSyntax {
 
@@ -19,8 +18,6 @@ object ValueModuleSpec extends MorphirBaseSpec with value.ValueSyntax {
   def listType(itemType: UType): UType = IrType.reference(FQName.fromString("Morphir.SDK:List:List"), itemType)
   val stringType: UType                = sdk.String.stringType
 
-  @nowarn("msg=discarded non-Unit value")                             // FIX:  Can't seem to shake this one
-  @nowarn("msg=a pure expression does nothing in statement position") // FIX: This one either
   def spec = suite("Value Module")(
     suite("Collect Variables should return as expected for:")(
       test("Apply") {
