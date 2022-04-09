c893b1d21e037a946839c1e4ff4ad802593df353
Scala Steward <me@scala-steward.org>
2022-04-09 08:55:14 +0200

    Update zio, zio-test, zio-test-sbt to 2.0.0-RC5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
     val scalafmt                  = "3.5.0"
     val `scala-java-time`         = "2.4.0-M2"
     val `zio-cli`                 = "0.2.2"
-    val zio                       = "2.0.0-RC2"
+    val zio                       = "2.0.0-RC5"
     val `zio-json`                = "0.3.0-RC3"
     val `zio-morphir`             = "0.1.0-M01"
     val `zio-parser`              = "0.1.3"
