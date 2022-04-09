f13f045adb22ad3876eeaf98a02fba14cd52671d
Scala Steward <me@scala-steward.org>
2022-04-09 06:43:41 +0200

    Update zio-json to 0.3.0-RC6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
     val `scala-java-time`         = "2.4.0-M2"
     val `zio-cli`                 = "0.2.2"
     val zio                       = "2.0.0-RC2"
-    val `zio-json`                = "0.3.0-RC3"
+    val `zio-json`                = "0.3.0-RC6"
     val `zio-morphir`             = "0.1.0-M01"
     val `zio-parser`              = "0.1.3"
     val `zio-prelude`             = "1.0.0-RC10"
