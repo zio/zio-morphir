a90d84dbed5340c2df3fd4e45fec908b07cbc4e2
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2022-04-11 17:39:16 -0400

    Fix type annotation

--- morphir-ir/shared/src/test/scala/zio/morphir/ir/sdk/CommonSpec.scala
+++ morphir-ir/shared/src/test/scala/zio/morphir/ir/sdk/CommonSpec.scala
@@ -1,13 +1,13 @@
 package zio.morphir.ir.sdk
 
+import zio.Scope
 import zio.morphir.ir.Type.Type
 import zio.morphir.ir.{Gens, Name, Path}
 import zio.morphir.testing.MorphirBaseSpec
 import zio.test._
-import zio.{Scope, ZIOAppArgs}
 
 object CommonSpec extends MorphirBaseSpec {
-  def spec: ZSpec[Environment with TestEnvironment with ZIOAppArgs with Scope, Any] = suite("Common Spec")(
+  def spec: ZSpec[TestEnvironment with Scope, Any] = suite("Common Spec")(
     suite("packageName")(
       test("should return the expected value") {
         assertTrue(Common.packageName.toPath == Path.fromString("Morphir.SDK"))
